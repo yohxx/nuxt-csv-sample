c166fb28197877efc9ddd10cbb59e93f4c797fd1
高橋洋平 <takahashi@hamee-consulting.co.jp>
2020-03-20 11:38:11 +0000

    🚧 CSVアップロードフォームサンプル追加

--- csv/pages/index.vue
+++ csv/pages/index.vue
@@ -1,53 +1,105 @@
 <template>
   <section class="section">
-    <div class="columns is-mobile">
-      <card
-        title="Free"
-        icon="github-circle"
+    <b-field>
+      <b-upload
+        v-model="dropFile"
+        drag-drop
       >
-        Open source on <a href="https://github.com/buefy/buefy">
-          GitHub
-        </a>
-      </card>
-
-      <card
-        title="Responsive"
-        icon="cellphone-link"
-      >
-        <b class="has-text-grey">
-          Every
-        </b> component is responsive
-      </card>
-
-      <card
-        title="Modern"
-        icon="alert-decagram"
-      >
-        Built with <a href="https://vuejs.org/">
-          Vue.js
-        </a> and <a href="http://bulma.io/">
-          Bulma
-        </a>
-      </card>
-
-      <card
-        title="Lightweight"
-        icon="arrange-bring-to-front"
-      >
-        No other internal dependency
-      </card>
+        <section class="section">
+          <div class="content has-text-centered">
+            <p>
+              <b-icon
+                icon="upload"
+                size="is-large"
+              />
+            </p>
+            <p>ファイルをドロップするか、ここをクリックしてアップロードするファイルを選択してください。</p>
+          </div>
+        </section>
+      </b-upload>
+    </b-field>
+    <div v-if="dropFile" class="block">
+      <div class="tags">
+        <span class="tag is-primary">
+          {{ dropFile.name }}
+          <button
+            class="delete is-small"
+            type="button"
+            @click="deleteDropFile(index)"
+          />
+        </span>
+      </div>
+      <button class="button is-primary" @click="uploadedData()">
+        読み込む
+      </button>
+    </div>
+    <div class="table-wrapper">
+      <table class="table">
+        <thead>
+          <tr>
+            <th>氏名</th>
+            <th>カタカナ</th>
+            <th>生年月日</th>
+            <th>会社名</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in csvData" :key="item.id">
+            <td>{{ item.lastname }} {{ item.firstname }}</td>
+            <td>{{ item.lastkana }} {{ item.firstkana }}</td>
+            <td>{{ item.birthday }}</td>
+            <td>{{ item.company }}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </section>
 </template>
 
 <script>
-import Card from '~/components/Card'
-
 export default {
-  name: 'HomePage',
+  name: 'UploadForm',
+  data () {
+    return {
+      dropFile: null,
+      csvData: [] // csvデータ格納用
+    }
+  },
+  methods: {
+    // アップロードしたファイル削除用
+    deleteDropFile (index) {
+      this.dropFile = null
+    },
+    // アップロードしたcsvファイルをcsvDataに格納する用
+    uploadedData () {
+      console.log("start upload..");
+      this.csvData = [] // csvDataを初期化
+      const csvfile = this.dropFile
+      const reader = new FileReader()
 
-  components: {
-    Card
+      const loadCSV = () => {
+        const lines = reader.result.split('\n') // 改行毎にデータを分ける
+        lines.shift() // csvファイルの先頭（ヘッダ）を削除
+        // csvファイルの各行をcsvDataにオブジェクトとしてpushする
+        lines.forEach((element, index) => {
+          const workerData = element.split(',') // 区切り文字はカンマ
+          if (workerData.length !== 6) { return } // 読み込んだ行のデータが欠けている場合は無視
+          this.csvData.push(
+            {
+              id: index,
+              lastname: workerData[0],
+              firstname: workerData[1],
+              lastkana: workerData[2],
+              firstkana: workerData[3],
+              birthday: new Date(workerData[4]),
+              company: workerData[5]
+            }
+          )
+        })
+      }
+      reader.onload = loadCSV
+      reader.readAsText(csvfile)
+    }
   }
 }
 </script>
